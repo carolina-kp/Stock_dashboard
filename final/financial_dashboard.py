6b0aa22441eacd717aebde302628b9cd81d6cf73
carolina-kp <carolinakogan04@gmail.com>
2025-05-18 22:08:04 +0200

    Update financial_dashboard.py to fix issue with css conectivity

--- final/financial_dashboard.py
+++ final/financial_dashboard.py
@@ -17,7 +17,7 @@ st.set_page_config(
 
 # Apply custom CSS
 def load_css():
-    with open("style.css") as f:
+    with open("final/style.css") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
     # Additional CSS directly in the code for immediate styling
